b959085fa150c511d5e569dda415fffb09d85513
Maikon Marques Rodrigues <markes544@hotmail.com>
2022-04-12 21:05:44 -0300

    pronto

---
+++ modulo1/variaveis/index.js
@@ -0,0 +1,60 @@
+// 1- Primeiro console ira imprimir 10 e no segundo ira imprimir 10 e 5
+
+/*let a = 10
+let b = 20
+c = b
+b = c
+a = b
+
+console.log(a, b, c)*/
+// 2 - A resposta sera 20 devido o valor de b ser o ultimo a ser citado
+
+// 3 - p = horasDiariasTrabalhadas t = valorDiaria 
+
+//1.1
+
+// A - 
+//let nome  = undefined
+
+// B -
+//let idade = undefined
+
+// C -
+//console.log(typeof nome, typeof idade)
+
+// D -
+// Undefined. Nao tem definicao pois nao criei valores para elas 
+
+// E -
+let nome  = prompt("Qual o seu nome?")
+let idade = prompt("Qual sua idade?")
+
+// F - 
+console.log(typeof nome, typeof idade)
+// Ambas sao String
+
+//  G -
+console.log("Olá",nome,"você tem",idade, "anos.")
+
+// 2 .2
+
+// A -
+let pergunta1 = "Você está usando uma roupa azul hoje? - Sim!" 
+let pergunta2 = "Você está usando uma roupa vermelha hoje? - Sim!"
+let pergunta3 = "Você está usando uma roupa amarela hoje? - Sim!"
+
+// B - 
+console.log(pergunta1, pergunta2, pergunta3)
+
+// 3.3
+let a = 10
+let b = 25
+
+c = a
+a = b
+b = c 
+
+console.log("O novo valor de a é", a) 
+console.log("O novo valor de b é", b) 
+
+
