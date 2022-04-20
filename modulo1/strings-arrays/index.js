a29813ce563af4545173a9b24475b8ccea5b5295
Maikon Marques Rodrigues <markes544@hotmail.com>
2022-04-19 21:36:55 -0300

    pronto

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,69 @@
+//___________________EXERCICIO 1_________________________
+/*A- undefined
+B- null
+C- 11
+D- 0
+E- 19 substiuindo o 4 
+F- 9
+*/
+
+/*let array
+console.log('a. ', array)
+
+array = null
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c. ', array.length)
+
+let i = 0
+console.log('d. ', array[i])
+
+array[i+4] = 19
+console.log('e. ', array)
+
+const valor = array[i+6]
+console.log('f. ', valor)*/
+
+// 2-
+/*const frase = prompt("Digite uma frase")
+
+console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)*/
+
+// SUBI NUM ONIBUS EM MIRROCOS  27
+
+//-------------------EXERCICIO 2------------------------------------
+
+/*let nomeUsuario = prompt("Qual seu nome?")
+let emailDoUsuario = prompt("Qual seu e-mail?")
+
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o) ${nomeUsuario}!`)*/
+
+//let comidasTops = ["hamburguer,\n", " sushi,\n", " frutas,\n", " café,\n", " Xzão,\n"]
+
+//console.log(comidasTops)
+
+/*console.log(`Essas são minhas comidas preferidas: ${comidasTops}` )
+let i = 0
+let comidaUsuario = prompt("Qual sua comida preferida?")
+comidasTops [i+1] = comidaUsuario
+console.log(comidasTops)*/
+
+//--------------------EXERCICIO 3----------------------------------
+
+/*let t1 = prompt("Lista de tarefas 1?")
+let t2 = prompt("Lista de tarefas 2?")
+let t3 = prompt("Lista de tarefas 3?")
+let listaDeTarefas = [t1, t2, t3]
+console.log(listaDeTarefas)
+let tPronto = Number(prompt("Qual tarefa você realizou?"))
+listaDeTarefas.splice(tPronto-1, 1)
+
+console.log(listaDeTarefas)*/
+
+let comidas =["banana", "morango", "abacaxi", "laranja", "ameixa"]
+
+
+
+
+
